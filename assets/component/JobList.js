9a6d772356060e06ff682a7f85c224d74039dee8
Cong <conbo1102a@gmail.com>
2022-10-14 13:15:16 +0700

    update project v1

--- assets/component/JobList.js
+++ assets/component/JobList.js
@@ -1,31 +1,20 @@
 import html from '../lib/core.js'
+import { connect } from '../js/store.js'
 
-function JobList() {
+function JobList({ experiences }) {
     return html`
         <ul class="job-list">
-            <li class="job-item animation-box">
-                <div class="content-box">
-                    <img src="./assets/img/html-5.png" alt="" class="job-img">
-                    <h4 class="job-name">HTML5 & CSS3</h4>
-                    <p class="job-description">Lorem ispum, or lipum as it is sometimes known, is dummy text used in laying out print, graphic or web design</p>
-                </div>
-            </li>
-            <li class="job-item animation-box">
-                <div class="content-box">
-                    <img src="./assets/img/javascript.png" alt="" class="job-img">
-                    <h4 class="job-name">Javascript</h4>
-                    <p class="job-description">Lorem ispum, or lipum as it is sometimes known, is dummy text used in laying out print, graphic or web design</p>
-                </div>
-            </li>
-            <li class="job-item animation-box">
-                <div class="content-box">
-                    <img src="./assets/img/java.png" alt="" class="job-img">
-                    <h4 class="job-name">Java</h4>
-                    <p class="job-description">Lorem ispum, or lipum as it is sometimes known, is dummy text used in laying out print, graphic or web design</p>
-                </div>
-            </li>
+            ${experiences.map( experience => ` 
+                <li class="job-item animation-box">
+                    <div class="content-box">
+                        <img src="${experience.jobImage}" alt="" class="job-img">
+                        <h4 class="job-name">${experience.jobName}</h4>
+                        <p class="job-description">${experience.jobDescription}</p>
+                    </div>
+                </li>
+            `)}
         </ul>
     `
 }
 
-export default JobList
+export default connect()(JobList)
--- assets/js/reducer.js
+++ assets/js/reducer.js
@@ -1,5 +1,32 @@
 const init = {
-
+    myImg: './assets/img/me.png',
+    experiences: [
+        {
+            jobImage: './assets/img/html-5.png',
+            jobName: 'HTML5 & CSS3',
+            jobDescription: 'Lorem ispum, or lipum as it is sometimes known, is dummy text used in laying out print, graphic or web design'
+        },
+        {
+            jobImage: './assets/img/javascript.png',
+            jobName: 'JAVASCRIPT',
+            jobDescription: 'Lorem ispum, or lipum as it is sometimes known, is dummy text used in laying out print, graphic or web design'
+        },
+        {
+            jobImage: './assets/img/java.png',
+            jobName: 'JAVA',
+            jobDescription: 'Lorem ispum, or lipum as it is sometimes known, is dummy text used in laying out print, graphic or web design'
+        }
+    ],
+    memoryAlbum: [
+        './assets/img/img1.jpg',
+        './assets/img/img2.jpg',
+        './assets/img/img3.jpg',
+        './assets/img/img4.jpg',
+        './assets/img/img5.jpg',
+        './assets/img/img6.jpg',
+        './assets/img/img7.jpg',
+        './assets/img/img8.jpg',
+    ]
 }
 
 const actions = {
